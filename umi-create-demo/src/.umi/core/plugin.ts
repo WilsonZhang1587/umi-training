a1f77858c47a2d44e4333f53e6900b3d8b6982e8
wilson <s28281771@gmail.com>
2021-01-03 17:24:37 +0800

    first commit

---
+++ umi-create-demo/src/.umi/core/plugin.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Plugin } from 'C:/Users/wilson.chang.p.m/Desktop/umi-demo/node_modules/@umijs/runtime';
+
+const plugin = new Plugin({
+  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','getInitialState','request',],
+});
+
+export { plugin };
---
+++ umi-create-demo/src/.umi/plugin-helmet/exports.ts
@@ -0,0 +1,3 @@
+// @ts-nocheck
+// @ts-ignore
+export { Helmet } from 'C:/Users/wilson.chang.p.m/Desktop/umi-demo/node_modules/react-helmet';
